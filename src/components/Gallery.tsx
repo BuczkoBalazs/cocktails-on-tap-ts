1546a603e4c4d60bfbd74a7068442a372a967fae
BuczkoBalazs <buci85@gmail.com>
2022-08-23 10:27:30 +0200

    Developement branch created.

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Cocktail } from './Cocktail';
 
+// type-ra átírni
 interface CocktailDetails {
     id: string,
     name: string,
@@ -12,8 +13,9 @@ interface CocktailDetails {
 
 export const Gallery = () => {
 
+    // típusokat beírni => enum
     const [cocktails, setCocktails] = useState<CocktailDetails[] | []>([]);
-    const [searchInput, setSearchInput] = useState('');
+    const [searchInput, setSearchInput] = useState<'asc' | 'desc' | ''>('');
     const [sortByName, setSortByName] = useState('asc');
     const [sortButton, setSortButton] = useState('Sort ascending')
 
@@ -31,16 +33,17 @@ export const Gallery = () => {
 
     const inputChangeHandle = (e: React.ChangeEvent<HTMLInputElement>) => setSearchInput(e.target.value);
 
+    // arrow func, setCocktails() belsejét kiszervezni.
     function sortButtonChangeHandle() {
         setCocktails([...cocktails.sort( (a,b) => sortByName === 'asc' ? a.name.localeCompare(b.name) : b.name.localeCompare(a.name))]);
         setSortByName(sortByName === 'asc' ? 'desc' : 'asc');
         setSortButton(sortButton === 'Sort ascending' ? 'Sort descending' : 'Sort ascending');
     }
-
+    /* react comp-ba kiszervezni a div-et, komponens-t*/
     return (
     <div className='gallery-wrapper'>
-        <h1>Welcome to our gallery! Feel free to browse.</h1>
         <div className='filters'>
+        <h1>Welcome to our gallery! Feel free to browse.</h1>
             <label>Search by name</label>
             <input type="text" name="search" id="search" value={searchInput} onChange={inputChangeHandle} />
             <button className='sort-by-name' onClick={sortButtonChangeHandle} >{sortButton}</button>
