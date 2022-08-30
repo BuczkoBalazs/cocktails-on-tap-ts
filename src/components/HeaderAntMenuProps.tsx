a0ae5b76323a256539348266298c62f641e3c1d8
BuczkoBalazs <buci85@gmail.com>
2022-08-30 10:40:09 +0200

    Tabs corrected in HeaderAntMenuProps component.

--- src/components/HeaderAntMenuProps.tsx
+++ src/components/HeaderAntMenuProps.tsx
@@ -3,40 +3,40 @@ import { HomeOutlined } from '@ant-design/icons';
 
 
 export const logo: MenuProps['items'] = [
-    {
-        label: (
-          <a href="http://localhost:3000/" rel="noopener noreferrer">
-            Cocktails on tap
-          </a>
-        ),
-        key: 'home',
-    },
+  {
+    label: (
+      <a href="http://localhost:3000/" rel="noopener noreferrer">
+        Cocktails on tap
+      </a>
+    ),
+    key: 'home',
+  },
 ];
 
 export const items: MenuProps['items'] = [
-    {
-        label: (
-          <a href="http://localhost:3000/" rel="noopener noreferrer">
-            Home
-          </a>
-        ),
-        key: 'home',
-        icon: <HomeOutlined />,
-      },
-    {
-        label: (
-          <a href="http://localhost:3000/gallery" rel="noopener noreferrer">
-            Gallery
-          </a>
-        ),
-        key: 'gallery',
-      },
-    {
-        label: (
-          <a href="http://localhost:3000/favourites" rel="noopener noreferrer">
-            Favourites
-          </a>
-        ),
-        key: 'favourites',
-      },
+  {
+    label: (
+      <a href="http://localhost:3000/" rel="noopener noreferrer">
+        Home
+      </a>
+    ),
+    key: 'home',
+    icon: <HomeOutlined />,
+  },
+  {
+    label: (
+      <a href="http://localhost:3000/gallery" rel="noopener noreferrer">
+        Gallery
+      </a>
+    ),
+    key: 'gallery',
+  },
+  {
+    label: (
+      <a href="http://localhost:3000/favourites" rel="noopener noreferrer">
+        Favourites
+      </a>
+    ),
+    key: 'favourites',
+  },
 ];
