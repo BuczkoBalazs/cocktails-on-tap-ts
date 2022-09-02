50216fbcfed03783018ac139901240cf761918e2
BuczkoBalazs <buci85@gmail.com>
2022-09-02 12:25:46 +0200

    LandingSlideDetails now has key in LandingAnt.

--- src/components/LandingAnt.tsx
+++ src/components/LandingAnt.tsx
@@ -18,7 +18,7 @@ export const LandingAnt = () => {
     pauseOnHover={false}
     pauseOnDotsHover={true}
     >
-        {slides && slides.map( ({id, title, text}: LandingSlides) => <LandingSlideDetails id={id} title={title} text={text} />
+        {slides && slides.map( ({id, title, text}: LandingSlides) => <LandingSlideDetails key={id} id={id} title={title} text={text} />
         )}
     </LandingCarousel>
     )
