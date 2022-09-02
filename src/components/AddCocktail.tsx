b11a714ca21635d43cc7c9756de21529ec7835b0
BuczkoBalazs <buci85@gmail.com>
2022-09-02 09:31:53 +0200

    AddCocktail styles moved to AddCocktailStyle.

--- src/components/AddCocktail.tsx
+++ src/components/AddCocktail.tsx
@@ -1,10 +1,6 @@
-import React from 'react';
-import { Button, Form, Input, Select, Space } from 'antd';
+import { Button, Form, Input, Select } from 'antd';
 import 'antd/dist/antd.css';
-import styled from '@emotion/styled';
-import { CocktailDetails } from './types/Types';
-
-import background from '../image/landing4.jpg'
+import { AddForm, AddSpace } from './AddCocktailStyle';
 
 const { Option } = Select;
 
@@ -24,27 +20,6 @@ export const AddCocktail = () => {
     const onFinishFailed = (errorInfo: any) => {
       console.log('Failed:', errorInfo);
     };
-
-
-  const AddSpace = styled(Space)( () => ({
-    width: '100%',
-    height: '100vh',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundImage: `url(${background})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center'
-  }));
-
-  const AddForm = styled(Form)( () => ({
-    width: '600px',
-    height: '60vh',
-    marginTop: '100px',
-    padding: '50px',
-    background: 'rgba(255,255,255, 0.3)',
-    boxShadow: 'rgb(0 0 0 / 90%) 0px 2px 8px',
-    backdropFilter: 'blur(10px)'
-  }));
 
   return (
 
