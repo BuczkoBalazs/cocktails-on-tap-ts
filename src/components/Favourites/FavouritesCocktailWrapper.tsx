9c8a514cec2272fe882918701b2d0ebf51cf8988
BuczkoBalazs <buci85@gmail.com>
2022-09-16 14:30:23 +0200

    Favourites page handles error and loading states.

--- src/components/Favourites/FavouritesCocktailWrapper.tsx
+++ src/components/Favourites/FavouritesCocktailWrapper.tsx
@@ -1,3 +1,4 @@
+import { Button, Spin, Result } from 'antd';
 import { CocktailAnt } from '../CocktailCard/CocktailAnt';
 import 'antd/dist/antd.css';
 import { CocktailWrapperSpace } from './FavouritesAntStyle';
@@ -14,11 +15,18 @@ type CocktailDetails = {
 
 export const FavouritesCocktailWrapper = () => {
 
-    const { data: cocktails } = useFetch<CocktailDetails[]>('http://localhost:3001/cocktails');
+    const { data: cocktails, loading, error } = useFetch<CocktailDetails[]>('http://localhost:3001/cocktails');
 
     return (
     <CocktailWrapperSpace wrap={true}>
-        {cocktails && cocktails.map((cocktail: CocktailDetails) => cocktail.favourite && <CocktailAnt key={cocktail.id} cocktail={cocktail} />)}
+        {cocktails.length > 1 && cocktails.map((cocktail: CocktailDetails) => cocktail.favourite && <CocktailAnt key={cocktail.id} cocktail={cocktail} />)}
+        {loading && <Spin />}
+        {error && <Result
+            status="500"
+            title="500"
+            subTitle={error}
+            extra={<Button type="link" href='http://localhost:3000/'>Back Home</Button>}
+        />}
     </CocktailWrapperSpace>
     )
 }
