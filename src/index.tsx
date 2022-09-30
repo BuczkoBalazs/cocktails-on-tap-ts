086d20a7ab152b59b46e51dd085e63d4caa50a13
BuczkoBalazs <buci85@gmail.com>
2022-09-30 09:25:08 +0200

    Landingpage renamed to LandingPage

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { FavoritesAnt } from './components/Favorites/FavoritesAnt';
 import { GalleryAnt } from './components/Gallery/GalleryAnt';
 import { HeaderAnt } from './components/Header/HeaderAnt';
-import { LandingAnt } from './components/Landingpage/LandingAnt';
+import { LandingAnt } from './components/LandingPage/LandingAnt';
 import { RouteNotFound } from './components/ErrorPage/RouteNotFound';
 import { AddCocktail } from './components/AddCocktail/AddCocktail';
 import { LoginContextProvider } from './components/contexts/LoginContext';
