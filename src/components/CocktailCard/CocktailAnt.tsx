3e59747384a529b3b740bd143a49e52cb2953530
BuczkoBalazs <buci85@gmail.com>
2022-11-03 13:28:07 +0100

    Delete and favorite only available on logged users

--- src/components/CocktailCard/CocktailAnt.tsx
+++ src/components/CocktailCard/CocktailAnt.tsx
@@ -3,11 +3,15 @@ import { CocktailCard} from './CocktailAntStyle';
 import { CocktailDetailed } from './CocktailDetailed';
 import { CocktailCardButtons } from './CocktailCardButtons';
 import { Cocktail } from '../../generate/graphql';
+import { useContext } from 'react';
+import { LoginContext } from '../contexts/LoginContext';
 
 export const CocktailAnt = ({ cocktail }: { cocktail: Cocktail }) => {
 
+    const loginContext = useContext(LoginContext);
+
     return (
-    <CocktailCard hoverable title={cocktail.name} extra={<CocktailCardButtons cocktail={cocktail} />}>
+    <CocktailCard hoverable title={cocktail.name} extra={loginContext.user.name !== "Guest" && <CocktailCardButtons cocktail={cocktail} />}>
         <CocktailDetailed cocktail={cocktail} />
     </CocktailCard>
     )
