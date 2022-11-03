cc45a49e73a7be41cc228cb68fbc5cd0847d01ea
BuczkoBalazs <buci85@gmail.com>
2022-11-03 12:29:56 +0100

    Login now checks DB.

--- src/components/Header/Login.tsx
+++ src/components/Header/Login.tsx
@@ -1,15 +1,21 @@
 import React, { useContext, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { LoginContext } from '../contexts/LoginContext';
-import { Button, Input, Space } from 'antd';
+import { Button, Input, message, Space } from 'antd';
 import 'antd/dist/antd.css';
+import { useUsersQuery } from '../../generate/graphql';
 
 export const Login = () => {
 
     const loginContext = useContext(LoginContext);
 
+    const { data } = useUsersQuery();
+
     const handleLogin = () => {
-        // loginContext.setUser({name: input});
+        const userData = data?.users?.filter(user => user.email === input)
+        userData!.length >= 1 ?
+        loginContext.setUser({id: userData![0].id, name: userData![0].name}) :
+        message.error("This e-mail address is not registered!")
         setInput('');
     };
 
@@ -23,7 +29,7 @@ export const Login = () => {
 
     return (
     <Space>
-        {loginContext.user.name === 'Guest' && <Input type="text" value={input} onChange={inputChangeHandle} />}
+        {loginContext.user.name === 'Guest' && <Input type="text"    value={input} onChange={inputChangeHandle} />}
         {loginContext.user.name === 'Guest' && <Button onClick={handleLogin}>Login</Button>}
         {loginContext.user.name !== 'Guest' && <Button onClick={handleLogout}>Logout</Button>}
         {loginContext.user.name === 'Guest' && <Button onClick={() => navigate('/reg')}>Register</Button>}
